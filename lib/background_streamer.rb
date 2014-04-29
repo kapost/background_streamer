96af6a0505360298d057d3089196e9350d767919
Raul E Rangel <Raul@kapost.com>
2014-04-29 10:57:16 -0600

    Copy the Mongoid style of grabbing the logger

--- lib/background_streamer.rb
+++ lib/background_streamer.rb
@@ -1,14 +1,28 @@
 require 'logger'
 
 module BackgroundStreamer
-  if defined?(Rails)
-    @logger = Rails.logger
-  end
-
-  @logger ||= Logger.new(STDERR)
 
   class << self
-    attr_accessor :logger
+    def logger
+      return @logger if defined?(@logger)
+      @logger = rails_logger || default_logger
+    end
+
+    def logger=(logger)
+      @logger = logger
+    end
+
+    def rails_logger
+      defined?(Rails) && Rails.respond_to?(:logger) && Rails.logger
+    end
+
+    def default_logger
+      return @default_logger if defined?(@default_logger)
+
+      @default_logger = Logger.new(STDOUT)
+      @default_logger.level = @default_logger::INFO
+      @default_logger
+    end
   end
 end
 
